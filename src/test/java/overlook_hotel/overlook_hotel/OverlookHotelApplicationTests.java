290d2b1d8a524e0556c22b6b423a11193f871e85
Adeline <adeline.patenne@hotmail.fr>
2025-09-14 19:40:26 +0200

    feat: docker-compose.yml db service and app service functional
    init.sql create and populate db with fake data

---
+++ src/test/java/overlook_hotel/overlook_hotel/OverlookHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package overlook_hotel.overlook_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverlookHotelApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
