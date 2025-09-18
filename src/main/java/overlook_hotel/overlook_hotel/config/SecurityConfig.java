518aff73983db3e5175bc5ed3df3911bb70b13da
Florence <enolaworld@gmail.com>
2025-09-18 10:04:04 +0200

    feat: add spring boot actuator

--- src/main/java/overlook_hotel/overlook_hotel/config/SecurityConfig.java
+++ src/main/java/overlook_hotel/overlook_hotel/config/SecurityConfig.java
@@ -1,25 +1,28 @@
 package overlook_hotel.overlook_hotel.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.context.annotation.Bean;
 
 @Configuration
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/", "*/*", "/*", "/css/**", "*/filterForm", "/js/**").permitAll()
-//                        .anyRequest().authenticated()
-//                )
-//                .formLogin(login -> login.loginPage("/login").permitAll());
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
                 .csrf(csrf -> csrf.disable())
-                .formLogin(login -> login.disable())
+                .authorizeHttpRequests(auth -> auth
+                        // Actuator ouvert
+                        .requestMatchers("/actuator/**").permitAll()
+                        // assets / pages ouvertes
+                        .requestMatchers("/", "/clients/**", "/employees/**",
+                                "/css/**", "/js/**", "/images/**").permitAll()
+                        // et tout le reste aussi ouvert (pour l’instant)
+                        .anyRequest().permitAll()
+                )
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> {})     // optionnel
                 .logout(logout -> logout.disable());
 
         return http.build();
